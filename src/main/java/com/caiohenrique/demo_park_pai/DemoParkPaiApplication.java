e4c7fe6d874ef50dff81aa2c4d64f5d0959582b3
Caio Henrique <caiohenrique-dev@outlook.com>
2024-09-12 09:12:49 -0300

    projeto criado

---
+++ src/main/java/com/caiohenrique/demo_park_pai/DemoParkPaiApplication.java
@@ -0,0 +1,13 @@
+package com.caiohenrique.demo_park_pai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParkPaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParkPaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiohenrique/demo_park_pai/DemoParkPaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiohenrique.demo_park_pai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParkPaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
