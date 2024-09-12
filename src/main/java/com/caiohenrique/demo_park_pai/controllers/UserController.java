3dc9e3f2af8a293566111820d4e61cf4c2fedeb9
Caio Henrique <caiohenrique-dev@outlook.com>
2024-09-12 19:17:42 -0300

    crud completo finalizado

--- src/main/java/com/caiohenrique/demo_park_pai/controllers/UserController.java
+++ src/main/java/com/caiohenrique/demo_park_pai/controllers/UserController.java
@@ -5,10 +5,9 @@ import com.caiohenrique.demo_park_pai.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -21,6 +20,30 @@ public class UserController {
     public ResponseEntity<User> create(@RequestBody User user) {
        User newUser = userService.create(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody User user) {
+        userService.updatePassword(id, user.getPassword());
+        return ResponseEntity.noContent().build();
     }
 
 
--- src/main/java/com/caiohenrique/demo_park_pai/services/UserService.java
+++ src/main/java/com/caiohenrique/demo_park_pai/services/UserService.java
@@ -6,6 +6,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -14,6 +16,33 @@ public class UserService {
     @Transactional
     public User create(User user) {
         return userRepository.save(user);
+    }
+
+    public User findById(Long id) {
+        return userRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("USUÁRIO NÃO ENCONTRADO"));
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void updatePassword(Long id, String newPassword) {
+        User user = findById(id);
+        user.setPassword(newPassword);
+        userRepository.save(user);
+    }
+
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            userRepository.deleteById(id);
+        }catch (Exception exception) {
+            throw new RuntimeException("USUÁRIO NÃO PODE SER EXCLUÍDO, HÁ RELAÇÃO COM OUTRA ENTIDADE");
+        }
     }
 
 }
