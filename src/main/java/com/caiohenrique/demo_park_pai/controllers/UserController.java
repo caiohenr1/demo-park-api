2dabdc7903aec20ac3d9baf0061d03a4f179dccf
Caio Henrique <caiohenrique-dev@outlook.com>
2024-09-12 16:18:44 -0300

    alterado metodo create user

--- src/main/java/com/caiohenrique/demo_park_pai/controllers/UserController.java
+++ src/main/java/com/caiohenrique/demo_park_pai/controllers/UserController.java
@@ -1,7 +1,12 @@
 package com.caiohenrique.demo_park_pai.controllers;
 
+import com.caiohenrique.demo_park_pai.entities.User;
 import com.caiohenrique.demo_park_pai.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     public final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+       User newUser = userService.create(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+
 }
--- src/main/java/com/caiohenrique/demo_park_pai/services/UserService.java
+++ src/main/java/com/caiohenrique/demo_park_pai/services/UserService.java
@@ -1,6 +1,8 @@
 package com.caiohenrique.demo_park_pai.services;
 
+import com.caiohenrique.demo_park_pai.entities.User;
 import com.caiohenrique.demo_park_pai.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,9 @@ import org.springframework.stereotype.Service;
 public class UserService {
     public final UserRepository userRepository;
 
+    @Transactional
+    public User create(User user) {
+        return userRepository.save(user);
+    }
 
 }
